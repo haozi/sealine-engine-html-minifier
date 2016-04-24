cfd859a213001bdce453fefd7f2de617cabbd675
浩知 <haozhi.lyj@alibaba-inc.com>
2016-04-24 16:06:03 +0800

    init

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var htmlmin = require('html-minifier').minify;
 var pkg = require('./package.json');
 var logger = require('sealine.logger')(pkg.name);
 
+try {
+  require('html-minifier');
+} catch (e) {
+  logger.error('please run `npm install html-minifier --save`')
+}
+var htmlmin = require('html-minifier').minify;
 module.exports = function(code, config) {
   return new Promise((resolve, reject) => {
     try {
